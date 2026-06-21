109ed1ae0911c0e676df36ce632af03ee478f4e9
jcnr1313 <70799300+jcnr1313@users.noreply.github.com>
2026-06-21 15:27:53 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'lafe-asc-v1';
+const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
